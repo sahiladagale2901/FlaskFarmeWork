aeabb21b48290ff33e8535eb5d1a3da767e5f40b
sahiladagale2901 <sahiladagale2801@gmail.com>
2025-09-20 00:09:14 +0530

    Streamlit Basic

---
+++ Streamlit/classification.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+
+
+@st.cache_data
+def load_data():
+    iris = load_iris()
+    df = pd.DataFrame(iris.data, columns=iris.feature_name)
+    df['species'] = iris.target
+    return df, iris.target_names
+
+df, target_name = load_data()
+
+model = RandomForestClassifier()
+model.fit(df.iloc[:, :-1], df['species'])
